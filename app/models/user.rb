c9157501444fc68313540b0759b1bfb6b8de67be
Giru-eng <hhara.ally@gmail.com>
2021-05-01 09:16:01 +0900

    新規登録/ログイン/ログアウトの実装
    -正規表現＋validationでエラーメッセージ表示
    -deviceのstrong parameter以外のデータ保存許可設定
    -registration、sessionとheaderのview編集
    -ER図微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers" }
+  validates :nickname, presence: true
+  validates :familyname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters" }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters" }
+  validates :familyname_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" }
+  validates :firstname_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" }
+  validates :birthday, presence: true
 end
