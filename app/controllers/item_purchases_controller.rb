95ceabfbe6bba63051391dea2eff7b195884fcac
Giru-eng <hhara.ally@gmail.com>
2021-05-08 15:18:17 +0900

    商品購入機能の実装(2)
    商品購入機能の実装の修正2回目

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -40,7 +40,7 @@ class ItemPurchasesController < ApplicationController
   def ensure_correct_user
     redirect_to root_path if current_user.id == @item.user_id && @item.item_purchase.blank? || @item.item_purchase.present?
     return
-    redirect_to root_path if authenticate_user! && @item.item_purchase.present?
+    redirect_to root_path if @item.item_purchase.present?
     return
   end
 end
